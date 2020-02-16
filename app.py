06edb6cba275ad910f522c1f0217fd64eeb5a49e
rocketpy <dimitr.ushakov@gmail.com>
2020-02-16 20:06:51 +0200

    modified

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+import json
+import urllib.request
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+#from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres1@localhost/DataCollector'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres1@localhost/DataCollector'  #  'sqlite:///blog.db'
 db = SQLAlchemy(app)
 
+class CommentPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+"""    
 class Data(db.Model):
-    #create a table
     __tablename__ = "data"
     id = db.Column(db.Integer, primary_key = True)
     height = db.Column(db.Integer)
@@ -22,12 +32,20 @@ class Data(db.Model):
         self.weight = weight
         self.shoesize = shoesize
         self.sex = sex
-
+"""
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/weather")
+def weather():
+    return render_template("weather.html")
+
+@app.route("/posts/<int:post_id>")
+def posts(post_id):
+    post = Blogpost.query.filter_by(id=post_id).one()
+    return render_template('posts.html', post=post)
 
 @app.route("/success", methods = ['POST'])
 def success():
