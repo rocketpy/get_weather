8632db14aa24fe2beb586d80e68883cd61486dc4
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-02-19 17:17:22 +0200

    added get_weather

--- get_weather/get_weather/spiders/scrap.py
+++ get_weather/get_weather/spiders/scrap.py
@@ -6,10 +6,10 @@ class MySpider(scrapy.Spider):
     start_urls = ['http://www.gismeteo.ua/city/daily/5093/']
 
     def parse(self, response):
-        #self.log('I just visited: ' + response.url)
         for item in response.css('div.values'):
             items = {
-                'day temperature': item.css('small.author::text').extract_first(),
-                'night temperature': item.css('span.text::text').extract_first()
+                'day temperature': response.css('small.author::text').extract(),
+                'night temperature': response.css('span.text::text').extract()
                     }
-            yield item
+            yield items
+
