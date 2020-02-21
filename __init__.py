799118616f348c28dabf96ffd513e6fb99b103b2
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-02-21 17:22:28 +0200

    added logout.html

--- __init__.py
+++ __init__.py
@@ -1,7 +1,27 @@
 from flask import Flask,  render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from .main import main as main_blueprint
+from .auth import auth as auth_blueprint
 
 
+app = Flask(__name__)
+db = SQLAlchemy(app)
+# app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db.init_app(app)
+
+# for auth routes
+app.register_blueprint(auth_blueprint)
+
+# for non-auth
+app.register_blueprint(main_blueprint)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+"""
 db = SQLAlchemy()
 
 
@@ -22,3 +42,8 @@ def create_app():
 
     return app
 
+
+if __name__ == '__main__':
+    create_app()
+
+"""
