a1999fe96e7aee22b02f4fddb1a04cdfe38432cd
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-02-22 22:16:21 +0200

    modified: auth.py, main.py, profile.html

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from .models import User
 from .models import User
 from .weather import db
@@ -59,5 +59,4 @@ def login_post():
         return redirect(url_for('auth.login'))
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
-
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 from .weather import db
 
 
@@ -11,5 +12,6 @@ def index():
 
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', name=current_user.name)
