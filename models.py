db58641b3b2d976636d7efede5752a7ec10ef2cc
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-02-24 22:39:49 +0200

    modified

--- models.py
+++ models.py
@@ -4,6 +4,18 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(20))
+    surname = db.Column(db.String(20))
+    sex = db.Column(db.String(20))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+
+
+class UserPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.String(50))
+    title = db.Column(db.String(50))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
