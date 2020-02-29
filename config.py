3047d27196db2f197be865023df021328cd6c473
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-02-29 15:03:45 +0200

    modified: config.py, weather.py

---
+++ config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:////C:/Users/admin/PycharmProjects/get_weather/get_weather/database.db'
+SECRET_KEY = 'Some secret key'
--- weather.py
+++ weather.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask, Blueprint,  render_template, request, current_app
 from flask_sqlalchemy import SQLAlchemy
-from .main.main import main as main_blueprint
-from .auth.auth import auth as auth_blueprint
 from .models import User, UserPost
 from flask_login import LoginManager
 from flask_admin import Admin
@@ -11,13 +9,19 @@ from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 
-app.register_blueprint(auth_blueprint)  # for auth routes
-app.register_blueprint(main_blueprint)  # for non-auth
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 admin = Admin(app)
 
+from .main import main as main_blueprint
+from .auth import auth as auth_blueprint
+
+app.register_blueprint(auth_blueprint)  # for auth routes
+app.register_blueprint(main_blueprint)  # for non-auth
+
+
 # app.config['SECRET_KEY'] = ''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db.init_app(app)
 
 login_manager = LoginManager()
@@ -35,4 +39,4 @@ admin.add_view(ModelView(UserPost, db.session))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
