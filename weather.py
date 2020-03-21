451db0ba5793d25216ede458a05a287cbcf0da82
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-03-21 23:59:31 +0200

    modified

--- weather.py
+++ weather.py
@@ -149,8 +149,6 @@ def login_post():
 @app.route('/weather')
 @login_required
 def show_weather():
-    day_temperature = '+5'
-    night_temperature = '0'
     return render_template('weather.html')
 
 
