4b1e30fa9a24c1b0e82516dec833efc9df8a07bf
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-03-24 00:59:01 +0200

    modified

--- get_weather/get_weather/spiders/scrap.py
+++ get_weather/get_weather/spiders/scrap.py
@@ -4,15 +4,25 @@ from ..items import GetWeatherItem
 
 class MySpider(scrapy.Spider):
     name = "weather_spider"
-    start_urls = ['http://www.gismeteo.ua/city/daily/5093/']
+    start_urls = ['https://www.gismeteo.ua/weather-zaporizhia-5093/']
 
     def parse(self, response):
         items = GetWeatherItem()
-        # values = response.css('div.values')
-        # for i in values:
-        night_temperature = response.css('span.unit unit_temperature_c::text')[0].extract()
-        day_temperature = response.css('span.unit unit_temperature_c::text')[1].extract()
-        items['night_temperature'] = night_temperature
-        items['day_temperature'] = day_temperature
+        values = response.css('div.values')
+        #night_temperature = response.css('div.value.span.unit unit_temperature_c::text')[0].extract()
+        #day_temperature = response.css('div.value.span.unit unit_temperature_c::text')[1].extract()
 
-        yield {'day_temperature': day_temperature, 'night_temperature': night_temperature}
+        for item in values:
+            night_temperature = item.css('div.value.span.unit unit_temperature_c::text')[0].extract()
+            day_temperature = item.css('div.value.span.unit unit_temperature_c::text')[1].extract()
+            # items['night_temperature'] = night_temperature
+            # items['day_temperature'] = day_temperature
+            print(night_temperature)
+            print(day_temperature)
+
+"""
+            yield {
+                'night_temperature': night_temperature,
+                'day_temperature': day_temperature}
+
+"""
