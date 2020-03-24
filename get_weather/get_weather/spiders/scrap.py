d397412c2663135a7ddfcbc8894fb42ca09a853a
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-03-24 21:43:16 +0200

    modified scrap.py

--- get_weather/get_weather/spiders/scrap.py
+++ get_weather/get_weather/spiders/scrap.py
@@ -8,20 +8,14 @@ class MySpider(scrapy.Spider):
 
     def parse(self, response):
         items = GetWeatherItem()
-        
-        items['night_temperature'] = night_temperature
-        items['day_temperature'] = day_temperature
         values = response.css('div.values')
-        #night_temperature = response.css('div.value.span.unit unit_temperature_c::text')[0].extract()
-        #day_temperature = response.css('div.value.span.unit unit_temperature_c::text')[1].extract()
 
         for item in values:
-            night_temperature = item.css('div.value.span.unit unit_temperature_c::text')[0].extract()
-            day_temperature = item.css('div.value.span.unit unit_temperature_c::text')[1].extract()
+            night_temperature = item.css('span.unit unit_temperature_c::text')[0].extract()
+            day_temperature = item.css('span.unit unit_temperature_c::text')[1].extract()
+            items['night_temperature'] = night_temperature
+            items['day_temperature'] = day_temperature
 
-"""
             yield {
                 'night_temperature': night_temperature,
                 'day_temperature': day_temperature}
-
-"""
