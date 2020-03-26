f291e5bde9b2bd76d979af1d82505c0d67b08ca0
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-03-26 21:18:21 +0200

    modified

--- get_weather/get_weather/spiders/scrap.py
+++ get_weather/get_weather/spiders/scrap.py
@@ -7,14 +7,13 @@ class MySpider(scrapy.Spider):
     start_urls = ['https://www.gismeteo.ua/weather-zaporizhia-5093/']
 
     def parse(self, response):
-        items = GetWeatherItem()
-        values = response.css('div.value')
+        # items = GetWeatherItem()
+        values = response.css('.value')
+        for val in values:
+            night_temperature = val.css('.unit.unit_temperature_c::text')[0].extract()
+            day_temperature = val.css('.unit.unit_temperature_c::text')[1].extract()
+            items = GetWeatherItem()
+            items['night_temperature'] = night_temperature
+            items['day_temperature'] = day_temperature
 
-
-        night_temperature = values.css('span.unit.unit_temperature_c::text')[0].extract()
-        day_temperature = values.css('span.unit.unit_temperature_c::text')[1].extract()
-
-        items['night_temperature'] = night_temperature
-        items['day_temperature'] = day_temperature
-
-        #yield items
+            yield items
