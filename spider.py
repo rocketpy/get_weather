1799864d8504fe7d6eb46f8c9fe34aa079831b26
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-03-29 22:44:00 +0300

    modified

--- spider.py
+++ spider.py
@@ -1,36 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-# import scrapy
-# from scrapy.crawler import CrawlerProcess
-
-night_temperature = []
-day_temperature = []
+import scrapy
+from scrapy.crawler import CrawlerProcess
 
 
-def get_html(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-    r = requests.get(url, headers=headers)
-    return r.text
-
-
-def get_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    night_temp = soup.find_all('div', {'class': 'value'})[0].find('span', {'class': "unit unit_temperature_c"}).text
-    day_temp = soup.find_all('div', {'class': 'value'})[1].find('span', {'class': "unit unit_temperature_c"}).text
-    night_temperature.append(night_temp)
-    day_temperature.append(day_temp)
-    print(night_temperature[-1])
-    print(day_temperature[-1])
-
-
-def main():
-    url = 'https://www.gismeteo.ua/weather-zaporizhia-5093/'
-    get_data(get_html(url))
-
-if __name__ == '__main__':
-    main()
-
-"""
 def show_weather():
     night_temperature = []
     day_temperature = []
@@ -57,4 +28,3 @@ def show_weather():
 
 if __name__ == '__main__':
     show_weather()
-"""
