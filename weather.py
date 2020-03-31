d5d8f6f69d2f0cef4b5f35962f40d23deccfc498
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-03-31 21:14:00 +0300

    updated

--- weather.py
+++ weather.py
@@ -60,9 +60,9 @@ class SignUp(FlaskForm):
                                            Length(min=2, max=20)])
     surname = StringField('surname', validators=[InputRequired(message='An surname is required !'),
                                                  Length(min=2, max=20)])
-    sex = StringField('sex', validators=[Length(min=4, max=6, message='Not greater a 6 simbols')])
     email = StringField('email', validators=[InputRequired(message='An email is required !'),
-                                             Length(min=10, max=50)])
+                                             Length(min=5, max=50)])
+    sex = StringField('sex', validators=[Length(min=4, max=6, message='Not greater a 6 simbols')])
     birthday = StringField('birthday', validators=[Length(min=6, max=10, message='Not greater a 6 simbols')])
     password = PasswordField('password', validators=[InputRequired(message='A password is required !'),
                                                      Length(min=5, max=50, message='Not greater a 50')])
@@ -70,7 +70,7 @@ class SignUp(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(message='An email is required !'),
-                                             Length(min=10, max=50)])
+                                             Length(min=5, max=50)])
     password = PasswordField('password', validators=[InputRequired(message='A password is required !'),
                                                      Length(min=5, max=50, message='Not greater a 50')])
     remember = BooleanField('remember me')
@@ -80,7 +80,7 @@ class AddPostForm(FlaskForm):
     name = StringField('name', validators=[InputRequired(message='An name is required !'),
                                            Length(min=2, max=20, message='Is a wrong length')])
     email = StringField('email', validators=[InputRequired(message='An email is required !'),
-                                             Length(min=10, max=50, message='It is a wrong length')])
+                                             Length(min=5, max=50, message='It is a wrong length')])
     message = StringField('message', validators=[InputRequired(message='Text field is required !'),
                                                  Length(min=5, max=1000, message='It is a wrong length')])
 
