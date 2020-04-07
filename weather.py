588d3c2caf33d419a4e31e5a12149f4814eb77da
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-04-07 21:56:44 +0300

    modified weather.py

--- weather.py
+++ weather.py
@@ -12,11 +12,6 @@ from wtforms.validators import InputRequired, Length
 from flask import render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_user import UserMixin, login_required, current_user
-# import scrapy
-# import socketio
-# from scrapy.crawler import CrawlerProcess
-# from scrapy.settings import CrawlerSettings
-# from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -28,10 +23,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 db.init_app(app)
 # app.config.from_pyfile('config.py')
-# app.config['USER_ENABLE_EMAIL'] = False
-# app.config['USER_APP_NAME'] = 'Flask_weather'
-# sio = socketio.CrawlerSettings()
-# sio.attach(app)
 
 
 class User(db.Model, UserMixin):
@@ -168,30 +159,9 @@ def show_weather():
         url = 'https://www.gismeteo.ua/weather-zaporizhia-5093/'
         get_data(get_html(url))
 
-    # by Scrapy
-    """
-    class MySpider(scrapy.Spider):
-        name = "weather_spider"
-        allowed_domains = ['gismeteo.ua']
-        start_urls = ['https://www.gismeteo.ua/weather-zaporizhia-5093/']
-
-        def parse(self, response):
-            values = response.css('div.value')
-            night_temp = values.css('span.unit.unit_temperature_c::text')[0].extract()
-            day_temp = values.css('span.unit.unit_temperature_c::text')[1].extract()
-            night_temperature.append(night_temp)
-            day_temperature.append(day_temp)
-
-    process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
-    process.crawl(MySpider)
-    process.start()
-    """
     main()
     return render_template('weather.html', night_temperature=night_temperature[-1],
                            day_temperature=day_temperature[-1])
-
-    # from scrapy import cmdline
-    # cmdline.execute("scrapy crawl myspider".split())
 
 
 @app.route('/add', methods=['POST', 'GET'])
@@ -235,10 +205,6 @@ def page_not_found(error):
 def internal_error(error):
     return render_template('500_error.html', title='500'), 500
 
-
-# app.register_blueprint(auth)
-# app.register_blueprint(main)
-
 login_manager = LoginManager()
 login_manager.login_view = 'login.html'
 login_manager.init_app(app)
@@ -255,6 +221,3 @@ admin.add_view(ModelView(UserPost, db.session))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# socketio.run(app, debug=False)
-# app.run(host='0.0.0.0', port=4000)   http://localhost:4000/
