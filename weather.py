04ef084da5137c7ae3157a60c0fa4e48ae2ea338
rocketpy <dimitr.ushakov@gmail.com>
2020-04-21 22:39:18 +0300

    updated API methods

--- weather.py
+++ weather.py
@@ -202,8 +202,15 @@ def post():
 
 @app.route('/user/<public_id>', methods=['GET'])
 def get_one_user(public_id):
-    
-    return ''
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message': 'Not found this user !'})
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+    return jsonify({'user': 'user_data'})
 
 
 @app.route('/user', methods=['GET'])
